31de8163a26c6e4d178d57767f99554ec7d805d5
Srijan Paul <srijan.paul@deepsource.io>
2022-02-04 21:51:19 +0530

    foo

--- js.js
+++ js.js
@@ -3,3 +3,32 @@ const x = require('node-serialize')
 x.unserialize("fooo");
 x.serialize({x: 1})
 x.serialize({ y : 1 })
+
+const test = false;
+
+const test = true
+
+
+function someFunction (notUsedParameter) {
+  return nonExistantVariable + 5 + 'cat' 
+}
+
+const anotherKindOfFunction = () => {
+  while (true) {
+    spamNonExistantFunction();
+  }
+}
+
+
+const badFunction () => {
+  while (true) {
+    spamNonExistantFunction();
+  }
+}
+
+
+let fruits = ['Apple', 'Banana', 'Cherry', 'Mango']
+function getFruit(index) {
+  index = index || 3 // Everybody likes mangoes
+  return fruits[index]
+}
