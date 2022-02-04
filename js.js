619900df5b8e904f2a08769eca2a29d5ce98ecd4
Srijan Paul <srijan.paul@deepsource.io>
2022-02-04 16:08:39 +0530

    x

--- js.js
+++ js.js
@@ -1,27 +1,4 @@
-import path from "path";
-import fs from "fs";
+const x = require('node-serialize')
 
-const unused_variable = 123;
-
-class Foo {
-  constructor(x) {
-    this.x = x;
-  }
-}
-
-path.join("a", "b");
-fs.readFile("xx");
-
-// unused object
-new Foo(10);
-
-function foo() {
-  "use strict";
-  return false;
-}
-
-
-let foo = 1
-console.log(foo)
-
-let x = 1
+x.unserialize("fooo");
+x.serialize({x: 1})
