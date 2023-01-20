d85f1072ae69c066405742383be0c30e76769369
Srijan Paul <srijan.paul@deepsource.io>
2023-01-20 12:26:34 +0530

    foo

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ import http from "http"
 import url from "url"
 
 http.createServer((req, _res) => {
-  const parsedUrl = url.parse(req.url, true)
-  // Vulnerable! user can inject special characters in the terminal
-  console.log(parsedUrl.query.username);
+  const q = url.parse(req.url, true)
+  console.log(q.query.username)
 })
