15fd1a84adabf9c4a5b27f424d8cd7e47353d9b8
Srijan Paul <srijan.paul@deepsource.io>
2023-01-27 11:38:25 +0530

    trigger run

--- index.js
+++ index.js
@@ -1,17 +1,3 @@
-import express from "express"
-
-const app = express()
-
-app.get("/user/:name", async (req, res) => {
-  const name = req.params.name
-  try {
-    const userJson = await getUserFromDb(name)
-    res.json(userJson)
-  } catch(err) {
-    res.status(404)
-    // The user will know the depdendencies and layout of
-    // the server's codebase.
-    res.send(err)
-    // Also harmful: res.send(err.stack), res.write(err.message) etc.
-  }
-})
+let a = 1;
+console.assert(a === 1) // ok
+console.log(a) // not ok
