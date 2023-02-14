4e3a98419252a77ad0a606948c9043bf39158277
Srijan Paul <srijan.paul@deepsource.io>
2023-02-14 12:05:06 +0530

    trigger analysis

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ export default function App() {
         <div className="my-dumb-div-3">
           <div className="stuff">
             <span>Actual content :)</span>
+            <span>Hello world</span>
+            <span>I love violence</span>
+            <span>:DDD</span>
           </div>
         </div>
       </div>
     </div>
   </div>
 }
+
