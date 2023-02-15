904d967a93ab9a80af68ac2c2cd638ee1a44be4a
Srijan Paul <srijan.paul@deepsource.io>
2023-02-15 16:02:47 +0530

    trigger analysis

--- index.js
+++ index.js
@@ -1,23 +1,5 @@
-for (let i = 0; i <= array.length; ++i) {
-  use(array[i]) // <- array[i] may be undefined
-}
+export const getDomainName = (hostName = 'https://ancestrystage.com/') => {
+  const re = /\.([^.]+?)$/;
+  return re.exec(hostName)[1];
+};
 
-console.assert(1 === 2);
-console.log(1 === 2);
-
-export class Something {
-  shouldBeStatic() {
-    // doesn't use [this]
-    return 123;
-  }
-}
-
-function main() {
-  return 0
-}
-
-function App() {
-  return 1;
-}
-
-main(); App();
