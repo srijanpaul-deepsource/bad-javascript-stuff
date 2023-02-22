c98c00c71f326d68e1b307baba7c3c8fadc42e45
Srijan Paul <srijan.paul@deepsource.io>
2023-02-22 12:46:52 +0530

    trigger analysis

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,4 @@
-import React from "react"
+import SomeComponent from "some-library";
 
-// [JS-0400]; [JS-0093]; [JS-0423]
-<Element foo={true} />;
+export default <SomeComponent personal={true} />;
 
-// [JS-0072]; [JS-0093]; [JS-0423]; [JS-0125]; [JS-0424]
-{<React.Fragment><Bar /></React.Fragment>}
-
-// [JS-0093]; [JS-0423]; [JS-0125]
-<Bar><Bar/></Bar>
-
-// [JS-0455]; [JS-0128]
-const HelloWorld = <div class="hello">Hello World</div>;
-
-// [JS-0128]
-const Hello = (props) => (
-    <div>Hello {props.name}</div>
-);
-
-// [JS-0128]
-const ContentContainer = <div className="content"></div>;
-
-// doc comment
-export function Stuff() {
-    return <div>
-        {/* [JS-0424] */}
-        <>[1,2,3]</>
-    </div>
-}
-
-export const MoreStuff = <React.Fragment>Should use shorthand fragments!</React.Fragment>
