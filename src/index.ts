3dcd9d0182cf321d6c1be3abc712a0d198a5fb7e
Srijan Paul <srijan.paul@deepsource.io>
2023-02-27 16:39:59 +0530

    trigger analysis

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,6 @@ import { Inject } from "some-module"
 // doc
 export class C {
   param: number
-  constructor(@Inject(1) param: number) {
-    this.param = param 
-  }
+  constructor(@Inject("SEQUELIZE") private readonly param: number) {}
 }
 
