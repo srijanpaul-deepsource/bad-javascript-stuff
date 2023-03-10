d9514f4f012b00182ad31dcf40e43efab4b9a577
Srijan Paul <srijan.paul@deepsource.io>
2023-03-10 14:53:11 +0530

    trigger analysis

--- index.js
+++ index.js
@@ -1,54 +1,13 @@
-import express from "express";
-import { Client } from "pg";
-import { initializeApp } from "firebase/app";
-import mysql from "mysql";
+const ob = {
+  set key(value) {
+    this._key = value;
+    return null;
+  },
 
-const app = express();
+  get key() {
+    return this._key
+  }
+}
 
-app.get("/resource", (req, res) => {
-  const cookieValue = "user-query:" + req.query;
-  res.setHeader("Set-Cookie", cookieValue);
-});
-
-const pgClient = new Client({
-  /* ... */
-});
-
-app.get("/resource", async (req, _res) => {
-  const query = "select * from user where name = " + req.query.name;
-  const result = await pgClient.query(query);
-  console.log(result);
-});
-
-const firebaseApp = initializeApp({
-  apiKey: "<YOUR_API_KEY_AS_A_STRING_LITERAL>",
-  // ^ This setting here may be exposed in error message stack traces etc.
-  appId: "1:2729...",
-  projectId: "firebase-project",
-  databaseURL: "https://<app>.firebaseio.com",
-  // ...
-});
-
-const conn = mysql.createConnection({
-  host: "<url>",
-  user: "bob",
-  password: "ABc-07",
-});
-
-
-const rSpaces = /\\x1a/;
-
-switch (color) {
-   case 'red' : handle_red(color); break;
-   case 'blue' : handle_red(color);
-   INDIGO: handle_indigo(color);
- }
-
-function mult(a, b, c) {
-   return a * b * c;
- }
-
-mult(1, 2, 3, 4); // '4' will be ignored, and the result will be '6'.
-
-export default { firebaseApp, conn, rSpaces };
+export default ob
 
