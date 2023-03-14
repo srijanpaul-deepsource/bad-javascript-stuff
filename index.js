f901ae1c5d60deb52ba00c2a55b8e8e3b8891795
Srijan Paul <srijan.paul@deepsource.io>
2023-03-14 16:51:24 +0530

    trigger analysis

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const xs = [[1], [2], [3, 4]]
-const ys = xs.reduce((a, b) => {
-  a.concat(b);
-})
-
-const ys2 = ys.find(x => {
-  xs.concat(x) 
-})
-
-export default ys
 
---
+++ src/Vue.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    Hello, world!    
+  </div>
+</template>
+
+<script lang="ts">
+interface Emits {
+  (e: 'select', item: any): void
+  (e: 'select'): void
+}
+
+export default {}
+</script>
+
