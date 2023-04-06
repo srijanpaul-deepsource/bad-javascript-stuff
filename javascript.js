bfb4e855be249f59b307f5e6e0d7eda1d4b40938
Srijan Paul <srijan.paul@deepsource.io>
2023-04-06 13:15:17 +0530

    trigger analysis

--- javascript.js
+++ javascript.js
@@ -1,31 +1,6 @@
-//
-// dev test file for marvin-javascript
-import express from 'express'
-import errorhandler from 'errorhandler'
-
-const app = express()
-
-// This will enable the error handler in both
-// and production
-app.use(errorhandler())
-
-const dialogue = `"Journey before destination", said Dalinar.`
-const dialogue2 = `What is a 'Kwisatz Haderach'?`
-
-const dataStore = {
-  _data: null,
-  set data(value) {
-    if (this.isValid(value)) {
-      this._data = value
-      return true
-    }
-
-    // We're returning false to indicate that
-    // the `set` was unsuccessful, but this
-    // return value will *never* be usable.
-    return false
-  }
+const str = "BadMan"
+if (str.toUpperCase() === "foo") {
+  // empty because reasons
 }
 
-export default { dialogue, dialogue2, dataStore }
 
