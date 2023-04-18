952b44fe9971719b0af87e709989cd516c9438e8
Srijan Paul <srijan.paul@deepsource.io>
2023-04-18 13:57:41 +0530

    trigger analysis

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,6 @@
-export default class F {
-  render() {
-    return 1
-  }
-
-
-  f() {
-    return 2
-  }
+export default function App() {
+  return <>
+    {/* This is not a lone block, but a comment */}
+    foo bar
+  </>
 }
-
