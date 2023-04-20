14faffe93da4a3044e7e0533e10e03b30f2578fa
Srijan Paul <srijan.paul@deepsource.io>
2023-04-20 12:26:54 +0530

    trigger analysis

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,18 @@
 import { Enum } from './enum'
 
-// doc-comment
+// doc-comment :D
 export function func(en: Enum) {
   switch (en) {
     case Enum.A: func(Enum.B); break;
     case Enum.B: func(Enum.A); break;
+    case Enum.C: func(Enum.C); break;
   }
   return
 }
 
+
+// @ts-ignore
+export const invalid = 1 + false
+
+// @ts-ignore i'm dumb and this is ok
+export const invalid2 = 1 + false
