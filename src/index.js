09f82867e5868453a29e44b00f4188f4e51e914c
Srijan Paul <srijan.paul@deepsource.io>
2023-06-12 15:05:39 +0530

    trigger analysis

--- src/index.js
+++ src/index.js
@@ -10,8 +10,8 @@ function myFunc(a, b, c) {
 export const myExport = myFunc(12, 13, undefined);
 export const myExport2 = myFunc(1 + 3, undefined, undefined);
 export const myExport3 = myFunc(1 + 1, undefined, 3 + 1);
+export const myExport35 = myFunc(1 * 1, undefined, 3 + 1);
 export const myExport4 = myFunc((() => {
   return 123;
 })(), undefined, 3 + 1);
-
 
