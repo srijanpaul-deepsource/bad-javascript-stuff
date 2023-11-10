c67a34def83147932fe47321f488ae187e84e809
Srijan Paul <srijan.paul@deepsource.io>
2023-11-10 13:20:09 +0530

    trigger analysis

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,6 @@
-export const one = 1;
-export const one_or_two = one || 2;
+if (true) {
+  // foo bar
+}
 
-let x = 1;
-
-/* foo bar
-skipcq: JS-0295, JS-0372 */
-// @ts-ignore
+// skipcq: JS-123 // skipcq: JS-0356
+const xx = 1;
